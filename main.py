c5d0d51ea910b2244abcff9e4e923b7b5896437c
IdeaLim <wiuand@naver.com>
2022-08-03 00:49:02 +0900

    최신 데이터 업데이트 기능 추가

--- main.py
+++ main.py
@@ -1,22 +1,30 @@
 from datetime import datetime
 
 
+def update_data():
+    """Nasdaq 지수 데이터 최신화"""
+    realtime_nasdaq = yf.download(tickers='^IXIC', interval='1m', period='1d')
+    realtime_nasdaq.to_csv(f"./YfinanceData/nasdaq_{today_str}.csv")
+    return realtime_nasdaq
+
 def printlog(message, *args):
-    """인자로 받은 문자열을 파이썬 셸에 출력한다."""
+    """인자로 받은 문자열을 출력"""
     print(datetime.now().strftime('[%m/%d %H:%M:%S]'), message, *args)
 
 if __name__ == '__main__':
-    try:
-        buy_percent = 0.19
 
-        # Data 동기화
-        
+    try:
+        today_str = (date.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+        buy_percent = 0.19
 
         while True:
             t_now = datetime.now()
             t_start = t_now.replace(hour=21, minute=30, second=0, microsecond=0)
             t_sell = t_now.replace(hour=10, minute=20, second=0, microsecond=0)
             t_exit = t_now.replace(hour=10, minute=30, second=0, microsecond=0)
+
+            # Data 동기화
+            t_data = update_data()
 
         if t_start < t_now < t_sell :
             buy
